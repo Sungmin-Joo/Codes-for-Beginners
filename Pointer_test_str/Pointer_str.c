8958afe1a0128fecdc0d3a132cf9ee3778096281
Sung-Min Joo <wntjdals2015@naver.com>
2019-03-06 23:03:57 +0900

    Desktop_update

---
+++ Pointer_test_str/Pointer_str.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	char *A = "Hello";
+	printf("%p\n", A);
+	printf("%s\n", A);
+}
+//----------reulst----------
+//00A56B30
+//Hello
+//--------------------------
+//The above values ​​are different every time.
