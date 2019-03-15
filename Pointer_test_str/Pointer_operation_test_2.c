194e621b709e4e7f84178f8a7b20e28bc1cc5aad
Sung-Min Joo <wntjdals2015@naver.com>
2019-03-16 04:00:59 +0900

    Desktop_update

---
+++ Pointer_test_str/Pointer_operation_test_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+	char *str = "APPLE";
+	// => {'A','P','P','L','E','\0'}
+	char *ptr_str;
+	ptr_str = str;
+	printf("ptr_str : %p\n      =\nstr : %p\n", ptr_str, str);
+	/*
+	ptr_str : 00ED7614
+	=
+	str : 00ED7614
+	----------------------------------------
+	*/
+	printf("----------------------------------------\n");
+	printf("Result of str[0] = '%c'\n", str[0]);
+	printf("Result of *str = '%c'\n", *str);
+	printf("Result of *ptr_str = '%c'\n", *ptr_str);
+	/*
+	----------------------------------------
+	Result of str[0] = 'A'
+	Result of *str = 'A'
+	Result of *ptr_str = 'A'
+	----------------------------------------
+	The same result in different expressions
+	*/
+	printf("----------------------------------------\n");
+	printf("Result of str[0]+1 = '%c'\n", str[0] + 1);
+	printf("Result of *ptr_str+1 = '%c'\n", *ptr_str + 1);
+	printf("Result of *(ptr_str + 1) = '%c'\n", *(ptr_str + 1));
+	printf("----------------------------------------\n");
+	/*
+	----------------------------------------
+	Result of str[0]+1 = 'B'
+	Result of *ptr_str+1 = 'B'
+	Result of *(ptr_str + 1) = 'P'
+	----------------------------------------
+	str[0] + 1 => 'A' + 1 => 'B'
+	*ptr_str + 1 => 'A' + 1 => 'B'
+	*(ptr_str + 1) => str[1] => 'P'
+	*/
+	return 0;
+}
