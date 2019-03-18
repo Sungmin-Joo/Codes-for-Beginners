df637709e33ed91f9907048eafdf062757e776ab
Sung-Min Joo <wntjdals2015@naver.com>
2019-03-18 19:22:57 +0900

    Update hex_to_decimal.c

--- Notation_argorithm/hex_to_decimal.c
+++ Notation_argorithm/hex_to_decimal.c
@@ -5,32 +5,39 @@
 #include <stdio.h>
 #include <math.h>
 
+
 int hex_to_deciaml(char A)
 {
 	switch (A)
 	{
 	case 'F':
+	case 'f':
 		return 15;
 		break;
 	case 'E':
+	case 'e':
 		return 14;
 		break;
 	case 'D':
+	case 'd':
 		return 13;
 		break;
 	case 'C':
+	case 'c':
 		return 12;
 		break;
 	case 'B':
+	case 'b':
 		return 11;
 		break;
 	case 'A':
+	case 'a':
 		return 10;
 		break;
-	default :
+	default:
 		return A - 48;
 		break;
-	//"A - 48" means "N - 48". if cha A = '3', then "A - 48" is "51 - 48". 
+		//"A - 48" means "N - 48". if cha A = '3', then "A - 48" is "51 - 48". 
 	}
 }
 
