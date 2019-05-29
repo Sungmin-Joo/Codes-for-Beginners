63b7391a5dc377f21e312e42a3b519961f8b2b44
Sung-Min Joo <wntjdals2015@naver.com>
2019-05-30 02:27:21 +0900

    Update hex_to_decimal.c

--- Notation_argorithm/hex_to_decimal.c
+++ Notation_argorithm/hex_to_decimal.c
@@ -6,6 +6,29 @@
 #include <math.h>
 
 
+int hex_to_deciaml(char A);
+
+void main(void) {
+	int decimal = 0;
+	char hex[20] = { 0 };
+	int val, i;
+	printf("Enter the hex number :");
+	gets(hex);
+
+	val = strlen(hex) - 1;
+	for (i = val; i >= 0; i--)
+	{
+		decimal += (hex_to_deciaml(hex[i]) * pow(16.0, val - i));
+		//The process of converting from hex to decimal.
+	}
+
+	printf("---------------result----------------\n");
+	printf("%d\n", decimal);
+	printf("-------------------------------------\n");
+
+	return 0;
+}
+
 int hex_to_deciaml(char A)
 {
 	switch (A)
@@ -40,30 +63,7 @@ int hex_to_deciaml(char A)
 		//"A - 48" means "N - 48". if cha A = '3', then "A - 48" is "51 - 48". 
 	}
 }
-
-int main()
-{
-	int decimal = 0;
-	char hex[20] = { 0 };
-
-	printf("Enter the hex number :");
-	gets(hex);
-
-	int val = strlen(hex) - 1;
-	for (int i = val; i >= 0; i--)
-	{
-		decimal += (hex_to_deciaml(hex[i]) * pow(16.0, val - i));
-		//The process of converting from hex to decimal.
-	}
-
-	printf("---------------result----------------\n");
-	printf("%d\n", decimal);
-	printf("-------------------------------------\n");
-
-	return 0;
-}
-/*
-Enter the hex number :F8CD3
+8CD3
 ---------------result----------------
 1019091
 -------------------------------------
