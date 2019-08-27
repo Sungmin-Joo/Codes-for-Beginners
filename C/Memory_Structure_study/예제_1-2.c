7c907eeb7a9d6ab8c4635978857b24a93f1cd227
Sung-Min Joo <wntjdals2015@naver.com>
2019-08-27 17:47:35 +0900

    update

--- C/Memory_Structure_study/예제_1-2.c
+++ C/Memory_Structure_study/예제_1-2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	int *ptr;
-	int a = 10;
-	ptr[0] = 10;
-	printf("%d\n", *ptr);
-	//ptr변수는 메모리가 할당되지않은 포인터
-	//아직 할당되지 않은 메모리에 index 접근을 시도하여 엑세스 위반이 발생
-	return 0;
+    int *ptr;
+    int a = 10;
+    ptr[0] = 10;
+    printf("%d\n", *ptr);
+    //ptr변수는 메모리가 할당되지않은 포인터
+    //아직 할당되지 않은 메모리에 index 접근을 시도하여 엑세스 위반이 발생
+    return 0;
 }
