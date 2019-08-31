835df305d872f965864dbedf972473c1336baa54
Sung-Min Joo <wntjdals2015@naver.com>
2019-09-01 00:24:04 +0900

    Update compare.py

--- Python/Parallel_programming/compare.py
+++ Python/Parallel_programming/compare.py
@@ -3,41 +3,32 @@ import time
 import multiprocessing
 
 def func(x):
-    result = 0
-    for i in x:
-        result += i
-    return result
-    
+    print(x)
+    time.sleep(2)
+    return
 
 if __name__ == '__main__' :
     
     
-    start = time.time()
-    p1 = [i for i in range(50000)]
-    p2 = [i for i in range(50000,100000)]
-    p3 = [i for i in range(100000,150000)]
-    p4 = [i for i in range(150000,200000)]
+    start = time.time()    
+    process_list = ['p1','p2','p3','p4']
     
-    process_list = [p1,p2,p3,p4]
-    answer = 1
     for _ in process_list:
-        answer *= func(_)
+        func(_)
     end = time.time()
     
     print('sequence programming : %s sec' %(end - start))
-    print('answer : %d' %(answer))
+    
     
     start = time.time()
-    answer=0
+    
     p = multiprocessing.Pool(processes=4)
-    answer = p.map(func,process_list)
+    p.map(func,process_list)
     p.close()
     p.join()
-    temp = 1
-    for i in answer:
-        temp*= i 
+    
     print('parallel programming : %s sec' %(time.time() - start))
-    print('answer : %d' %(temp))
+    
     
 '''
 ---------- result ----------
@@ -50,5 +41,5 @@ p1
 p2
 p3
 p4
-parallel programming : 2.4593899250030518 sec
+parallel programming : 2.3133899250030518 sec
 '''
