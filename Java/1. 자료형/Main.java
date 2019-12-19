806ad7831a2b830a85376e42a8f4ed9819da5703
Sung-Min Joo <big-joo_dev@naver.com>
2019-12-19 13:24:24 +0900

    add 12/19
    입사 전 자바 복습 시작

---
+++ Java/1. 자료형/Main.java
@@ -0,0 +1,35 @@
+package project;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Dev_Joo!");
+
+        //Java의 대표적인 자료형
+        //정수형
+        int intVar = 1;
+        short shortVar = 1;
+        long longVar = 1;
+
+        //실수형 - float은 f라는 글자로 명시를 해줘야 한다.
+        float floatVar = 1.2f;
+        double doubleVar = 334.256;
+
+        //Boolean 형
+        boolean bolleanVar = true;
+
+        //문자형
+        char charVar = 'c';
+
+        //Java의 변수와 상수
+        //변수
+        int num = 2;
+        System.out.println("num is variable -> " + num);
+        System.out.println("if \"num = 3;\"");
+        num = 3;
+        System.out.println("num is variable -> " + num);
+
+        //상수
+        int s_num = 10;
+        System.out.println("s_num is constants -> " + s_num);
+    }
+}
